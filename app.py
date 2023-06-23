8d611cbd683609cd02aef16edbe4c70131270065
Maaz Karim <MaazKarim02@gmail.com>
2023-06-24 02:00:19 +0530

    Added model

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-# model = pickle.load(open("model.pkl", "rb"))
+with open("models/log_reg.pkl", "rb") as file:
+    model = pickle.load(file)
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("forest_fire.html")
 
 
@@ -18,26 +19,19 @@ def predict():
     final = [np.array(int_features)]
     print(int_features)
     print(final)
-    # prediction=model.predict_proba(final)
-    # output='{0:.{1}f}'.format(prediction[0][1], 2)
+    prediction = model.predict_proba(final)
+    output = "{0:.{1}f}".format(prediction[0][1], 2)
 
-    output = "0.2"
     if output > str(0.5):
         return render_template(
             "forest_fire.html",
-            pred="Your Forest is in Danger.\nProbability of fire occuring is {}".format(
-                output
-            ),
-            bhai="kuch karna hain iska ab?",
+            pred=f"Your Forest is in Danger.\nProbability of fire occuring is {output}",
         )
-    else:
-        return render_template(
-            "forest_fire.html",
-            pred="Your Forest is safe.\n Probability of fire occuring is {}".format(
-                output
-            ),
-            bhai="Your Forest is Safe for now",
-        )
+
+    return render_template(
+        "forest_fire.html",
+        pred=f"Your Forest is safe.\n Probability of fire occuring is {output}",
+    )
 
 
 if __name__ == "__main__":

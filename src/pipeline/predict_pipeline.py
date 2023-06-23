848aa9f6fdbe31bc99efca7b90836281a74b111a
Maaz Karim <MaazKarim02@gmail.com>
2023-06-24 01:58:57 +0530

    Added pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import pandas as pd
+from src.exceptions import CustomException
+from src.util import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(file_path=model_path)
+            print("After Loading")
+            preds = model.predict(features)
+            return preds
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class CustomData:
+    def __init__(
+        self,
+        temperature: int,
+        oxygen: int,
+        humidity: int,
+    ):
+        self.temperature = temperature
+
+        self.oxygen = oxygen
+
+        self.humidity = humidity
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "temperature": [self.temperature],
+                "oxygen": [self.oxygen],
+                "humidity": [self.humidity],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
